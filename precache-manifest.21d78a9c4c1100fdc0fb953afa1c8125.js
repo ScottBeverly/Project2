8d944d2d7c45e7cb2cf1c8257aa71624a081a9d1
Scott beverly JR <scottbeverlyjr@Scotts-Air.fios-router.home>
2020-04-02 15:20:12 -0400

    Updates

---
+++ precache-manifest.21d78a9c4c1100fdc0fb953afa1c8125.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1968dc8459948d7a4e055ff2f5ccbf35",
+    "url": "/Project2/index.html"
+  },
+  {
+    "revision": "d920298649a4c59c9cf6",
+    "url": "/Project2/static/css/main.e49a4674.chunk.css"
+  },
+  {
+    "revision": "f6faa57cf221089154d1",
+    "url": "/Project2/static/js/2.fe70376e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Project2/static/js/2.fe70376e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d920298649a4c59c9cf6",
+    "url": "/Project2/static/js/main.00668e1d.chunk.js"
+  },
+  {
+    "revision": "65654c328888eaf42366",
+    "url": "/Project2/static/js/runtime-main.43c5b877.js"
+  }
+]);
